6438d6156a168c5cd067a4453ce0f5a8d57a3c44
jeddf <jedd@outlook.com>
2014-03-05 16:30:59 +0000

    added test for soup creation and passing script in baseclass __init__

--- RSSFeed.py
+++ RSSFeed.py
@@ -2,6 +2,8 @@ import urllib2
 from bs4 import BeautifulSoup
 
 class RSSobject(object):
+
     def __init__(self, url):
         f = urllib2.urlopen(url)
         self.raw = f.read()
+        self.soup = BeautifulSoup(self.raw)
--- RSS_test.py
+++ RSS_test.py
@@ -2,7 +2,12 @@ import unittest
 from RSSFeed import RSSobject
 
 class RSSTester(unittest.TestCase):
+
     def setUp(self):
         self.RSS = RSSobject('http://feeds.bbci.co.uk/news/rss.xml')
+
     def rss_raw_page_test(self):
         assert '<?xml' in self.RSS.raw
+
+    def rss_soup_page_test(self):
+        assert 'BBC' in self.RSS.soup.title.string
