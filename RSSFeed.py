f857293e9a2669a7cd2040b89d047498eacfd813
jeddf <jedd@outlook.com>
2014-03-27 13:37:00 +0000

    Placeholder

--- RSSFeed.py
+++ RSSFeed.py
@@ -2,23 +2,29 @@ import urllib2
 from bs4 import BeautifulSoup
 from collections import OrderedDict
 
-class RSSobject(object):    # RSS Base object
-    def __init__(self, url):
-        t = urllib2.urlopen(url)
-        self.raw = t.read()
-        self.soup = BeautifulSoup(self.raw)
+class RSSobject(object):
+     """Base RSS parser class.
+         Attributes:
+           raw (str): raw code of website pull.
+           soup (bs4.BeautifulSoup): Soup class instance of web pull.
+           averages (collections.OrderedDict): sorted dictionary of words           and their count in all articles of web pull.
+    """
+     def __init__(self, url):
+          t = urllib2.urlopen(url)
+          self.raw = t.read()
+          self.soup = BeautifulSoup(self.raw)
 
-    def average_words(self):
-        if self.articles:
-            self.averages = {}
-            for i in self.articles:
-                words = i['text'].split()
-                for t in words:
+     def average_words(self):
+          if self.articles:
+               self.averages = {}
+               for i in self.articles:
+                    words = i['text'].split()
+               for t in words:
                     if t in self.averages:
-                        self.averages[t] += 1
+                         self.averages[t] += 1
                     else:
-                        self.averages[t] = 1
-            self.averages = OrderedDict(sorted(self.averages.items(), key=lambda t: t[1]))
+                         self.averages[t] = 1
+          self.averages = OrderedDict(sorted(self.averages.items(), key=lambda t: t[1]))
 
 class ViceRSS(RSSobject):    # Vice Class tailored for site structure
     def article_split(self):    # Split feed into articles
