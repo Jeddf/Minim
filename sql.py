a891a442111eec73988650e4b0436ed378ccd1de
jeddf <jedd@outlook.com>
2014-04-03 05:59:48 +0100

    Schema initiation for persisting records from crawler

--- sql.py
+++ sql.py
@@ -4,10 +4,10 @@ import sqlite3
 with sqlite3.connect("counts1.db") as conn:
     cursin = conn.cursor()
     cursin.execute("""CREATE TABLE submits
-                   (id INTEGER PRIMARY KEY, source TEXT, date TEXT
+                   (id INTEGER PRIMARY KEY NOT NULL, source TEXT, date TEXT
                    );""")
     cursin.execute("""CREATE TABLE wordage
-                   (submitid INTEGER,
+                   (submitid INTEGER NOT NULL,
                    word TEXT,
                    count TEXT,
                    FOREIGN KEY(submitid) REFERENCES submits(id)
